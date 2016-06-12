fa56b2fb93f482ff2ab0b46b51c6e2fd0a9460e9
Robert Vargas <vargasvisuals@gmail.com>
2016-06-12 17:38:45 -0400

    module chapter pass

--- filter.js
+++ filter.js
@@ -1,15 +1,23 @@
 var fs = require('fs')
+var path = require('path')
+var myModules = require('./modules.js')
 
 function filterFiles(){
-  fs.readdir(process.argv[2], function(err, list){
-    if(err) throw error;
+
+  var filePath = process.argv[2]
+  var fileType = process.argv[3]
+
+  fs.readdir(filePath, function(err, files){
+    if(err) throw err;
     else{
 
-      var phpfiles = list + '.' + 'php'
-
-      console.log(phpfiles)
+      files.forEach(function(file){
+        if(path.extname(file) === '.' + fileType){
+          console.log(file)
+        }
+      })
     }
-  })
+  });
 }
 
-filterFiles();
+myModules();
